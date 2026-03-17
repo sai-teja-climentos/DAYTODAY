83f20102b33ea0639c820c74083bc71d6eab27a1
sai-teja-climentos <saiteja.b@climentos.com>
2026-03-17 15:30:14 +0530

    Sessionstorage++
    sign up and login  auto storage by using sessionstorage++

--- Pages/JS/index.js
+++ Pages/JS/index.js
@@ -1,27 +1,69 @@
-function LoginForm(){
-    
+function SignupForm() {
+
    event.preventDefault();
 
-   JS_Username = "HTML";
-
-   JS_Password = "CSS";
-
-    Username = document.getElementById("Username").value;
-
-    Password = document.getElementById("Password").value;
-
-    if(JS_Username==Username && JS_Password==Password){
-
-    //    alert("welcome to the new loning page..!")
-
-       window.location.href = "demo.htm";
-
-    }else{
-        //  alert("SORRY THE PAGE WAS NOT FOR YOU...! ERORR")
-       window.location.href = "erorr.htm";
+   signupUsername = document.getElementById("SignupUsername").value;
+   signupPassword = document.getElementById("SignupPassword").value;
 
 
-    }
+   if (signupUsername && signupPassword) {
+
+
+      sessionStorage.setItem("SignupUsername", signupUsername);
+
+      sessionStorage.setItem("SignupPassword", signupPassword);
+
+      window.location.href = "index_log_in.htm";
+
+   }
+   else {
+
+      alert("Plz fill the the input's")
+
+   }
+
+
+}
+
+function LoginForm() {
+
+   event.preventDefault();
+
+   Username = document.getElementById("Username").value;
+   Password = document.getElementById("Password").value;
+
+
+   StorageUsername = sessionStorage.getItem("SignupUsername");
+   StoragePassword = sessionStorage.getItem("SignupPassword");
+
+
+   if (Username == StorageUsername && Password == StoragePassword) {
+
+      window.location.href = "demo.htm";
+
+   } else {
+      window.location.href = "erorr.htm";
+   }
+
+
+
+   //  sessionStorage.setItem("Username",Username);
+   //  sessionStorage.setItem("Password",Password);
+
+   //  alert("Date Saved!");
+
+   //  if(JS_Username==Username && JS_Password==Password){
+
+   //  //    alert("welcome to the new loning page..!")
+
+   //     window.location.href = "demo.htm";
+
+   //  }else{
+   //      //  alert("SORRY THE PAGE WAS NOT FOR YOU...! ERORR")
+   //     window.location.href = "erorr.htm";
+
+
+   //  }
 
 }
 
